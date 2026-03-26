2abef130c0d5579bc3372ad4a4618bd2a2e57901
Ugo Matrangolo <ugo.matrangolo@gmail.com>
2026-03-26 21:57:05 +0000

    Add custom.el file

---
+++ custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("efcecf09905ff85a7c80025551c657299a4d18c5fcfedd3b2f2b6287e4edd659"
+     "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7"
+     "2b0fcc7cc9be4c09ec5c75405260a85e41691abb1ee28d29fcd5521e4fca575b"
+     "0f9a1b7a0f1d09544668297c1f04e5a5452ae1f4cf69f11b125f4cff1d54783d"
+     default))
+ '(package-selected-packages nil)
+ '(treesit-font-lock-level 4))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:foreground "slategray" :slant italic))))
+ '(font-lock-function-call-face ((t (:foreground "tomato"))))
+ '(font-lock-function-name-face ((t (:foreground "black"))))
+ '(font-lock-keyword-face ((t (:foreground "black" :slant normal :weight bold))))
+ '(font-lock-string-face ((t (:foreground "springgreen4"))))
+ '(font-lock-type-face ((t (:foreground "royalblue1" :weight normal))))
+ '(font-lock-variable-name-face ((t (:foreground "black")))))
